94479d7b52b9d9f0ebb12425839b0a57193209f4
ali da <birisierikli@hotmail.com>
2021-01-31 00:05:42 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IProductService product = new ProductManager(new InMemoryProductDal());
+            foreach (var item in product.GetAll())
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implemente eden bir sınıf her zaman veritabanı tablosudur
+    public interface IEntity
+    {
+
+    }
+}
